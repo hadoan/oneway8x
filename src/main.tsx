c970e13b8843a482567e0327e5d8855a9bd618e0
Ha Doan <ha.doanmanh@gmail.com>
2026-06-24 13:41:12 +0200

    chore: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import "./index.css";
@@ -8,6 +9,7 @@ createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
     <ThemeProvider defaultTheme="dark">
       <App />
+      <Analytics />
     </ThemeProvider>
   </HelmetProvider>
 );
